19885040d79d501cccdfc74e2ed6b01f97b56936
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 16:56:27 +0100

    project init

---
+++ src/main/java/com/bierfleisch/industrialmod/IndustrialModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bierfleisch.industrialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IndustrialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
