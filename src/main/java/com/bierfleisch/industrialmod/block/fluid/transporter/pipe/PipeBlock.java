144d87468a2812971741a0be5874ad61996bccfe
Nicholas <nici.bournes21@gmail.com>
2023-12-20 23:40:09 +0100

    add direction config for pipe block

--- src/main/java/com/bierfleisch/industrialmod/block/fluid/transporter/pipe/PipeBlock.java
+++ src/main/java/com/bierfleisch/industrialmod/block/fluid/transporter/pipe/PipeBlock.java
@@ -7,6 +7,14 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
@@ -14,15 +22,44 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class PipeBlock extends FluidContainerBlock {
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0, 6, 0, 16, 10, 16);
+    public static final BooleanProperty NORTH = Properties.NORTH;
+    public static final BooleanProperty EAST = Properties.EAST;
+    public static final BooleanProperty SOUTH = Properties.SOUTH;
+    public static final BooleanProperty WEST = Properties.WEST;
+    public static final BooleanProperty UP = Properties.UP;
+    public static final BooleanProperty DOWN = Properties.DOWN;
 
     public PipeBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.getDefaultState()
+                .with(NORTH, true)
+                .with(EAST, true)
+                .with(SOUTH, true)
+                .with(WEST, true)
+                .with(UP, true)
+                .with(DOWN, true)
+        );
     }
 
     @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+    public ActionResult onUseWithWrench(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+        switch (hit.getSide()) {
+            case NORTH -> world.setBlockState(pos, state.cycle(NORTH));
+            case EAST -> world.setBlockState(pos, state.cycle(EAST));
+            case SOUTH -> world.setBlockState(pos, state.cycle(SOUTH));
+            case WEST -> world.setBlockState(pos, state.cycle(WEST));
+            case UP -> world.setBlockState(pos, state.cycle(UP));
+            case DOWN -> world.setBlockState(pos, state.cycle(DOWN));
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
     }
 
     @Nullable
