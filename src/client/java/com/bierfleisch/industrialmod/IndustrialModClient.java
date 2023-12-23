f16c5a6df68761c9966b1285bafa63ebc51d95c7
Nicholas <nici.bournes21@gmail.com>
2023-12-23 15:22:03 +0100

    feat: make status bar render correctly
    render behind chat and automatically position above water bubbles

--- src/client/java/com/bierfleisch/industrialmod/IndustrialModClient.java
+++ src/client/java/com/bierfleisch/industrialmod/IndustrialModClient.java
@@ -1,15 +1,9 @@
 package com.bierfleisch.industrialmod;
 
-import com.bierfleisch.industrialmod.register.IndustrialModEventRegister;
 import com.bierfleisch.industrialmod.register.IndustrialModScreenRegister;
-import com.bierfleisch.industrialmod.render.RadiationHudOverlay;
-import com.bierfleisch.industrialmod.screen.LiquidContainerScreen;
 import com.bierfleisch.industrialmod.register.IndustrialModBlockRegister;
-import com.bierfleisch.industrialmod.register.IndustrialModScreenHandlerRegister;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
 
 public class IndustrialModClient implements ClientModInitializer {
@@ -19,8 +13,5 @@ public class IndustrialModClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(IndustrialModBlockRegister.TANK_BLOCK, RenderLayer.getCutout());
 
         IndustrialModScreenRegister.registerAll();
-        IndustrialModEventRegister.registerAll();
-
-        HudRenderCallback.EVENT.register(new RadiationHudOverlay());
     }
 }
--- src/client/java/com/bierfleisch/industrialmod/mixin/client/RenderHudStatusBarMixin.java
+++ src/client/java/com/bierfleisch/industrialmod/mixin/client/RenderHudStatusBarMixin.java
@@ -1,30 +1,39 @@
-package com.bierfleisch.industrialmod.render;
+package com.bierfleisch.industrialmod.mixin.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static com.bierfleisch.industrialmod.IndustrialMod.MOD_ID;
 
-public class RadiationHudOverlay implements HudRenderCallback {
+@Mixin(InGameHud.class)
+public class RenderHudStatusBarMixin {
     private static final Identifier EMPTY_RADIATION =
             new Identifier(MOD_ID, "textures/gui/sprites/hud/radiation/radiation_empty.png");
     private static final Identifier FULL_RADIATION =
             new Identifier(MOD_ID, "textures/gui/sprites/hud/radiation/radiation_full.png");
 
-    @Override
-    public void onHudRender(DrawContext drawContext, float tickDelta) {
-        MinecraftClient client = MinecraftClient.getInstance();
+    @Shadow private int scaledWidth;
 
-        if (client == null) return;
+    @Shadow private int scaledHeight;
 
-        int x = client.getWindow().getScaledWidth() / 2 + 10;
-        int y = client.getWindow().getScaledHeight() - 49;
+    @Shadow @Final private MinecraftClient client;
 
+    @Inject(at = @At("TAIL"), method = "renderStatusBars")
+    private void renderStatusBars(DrawContext drawContext, CallbackInfo info) {
+        int x = this.scaledWidth / 2 + 10;
+        int y = this.scaledHeight - 49;
+
+        /*
         drawContext.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
         RenderSystem.setShader(GameRenderer::getPositionTexProgram);
@@ -33,6 +42,7 @@ public class RadiationHudOverlay implements HudRenderCallback {
 
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
+         */
 
         try {
             if (client.player.isSubmergedInWater()) {
@@ -41,6 +51,10 @@ public class RadiationHudOverlay implements HudRenderCallback {
         }
         catch (NullPointerException e) {
 
+        }
+
+        for (int i = 0; i < 10; i++) {
+            drawContext.drawTexture(FULL_RADIATION, x + i * 8, y, 0, 0, 9, 9, 9, 9);
         }
 
         for (int i = 0; i < 10; i++) {
