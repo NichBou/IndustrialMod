de78df670b2091a1fe1cd94f1a9afbd70eb974e6
Nicholas <nici.bournes21@gmail.com>
2023-12-23 16:07:41 +0100

    ref: clean up RenderHudStatusBarMixin

--- src/client/java/com/bierfleisch/industrialmod/mixin/client/RenderHudStatusBarMixin.java
+++ src/client/java/com/bierfleisch/industrialmod/mixin/client/RenderHudStatusBarMixin.java
@@ -1,14 +1,13 @@
 package com.bierfleisch.industrialmod.mixin.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,15 +16,16 @@ import static com.bierfleisch.industrialmod.IndustrialMod.MOD_ID;
 
 @Mixin(InGameHud.class)
 public class RenderHudStatusBarMixin {
+    @Unique
     private static final Identifier EMPTY_RADIATION =
             new Identifier(MOD_ID, "textures/gui/sprites/hud/radiation/radiation_empty.png");
+
+    @Unique
     private static final Identifier FULL_RADIATION =
             new Identifier(MOD_ID, "textures/gui/sprites/hud/radiation/radiation_full.png");
 
     @Shadow private int scaledWidth;
-
     @Shadow private int scaledHeight;
-
     @Shadow @Final private MinecraftClient client;
 
     @Inject(at = @At("TAIL"), method = "renderStatusBars")
@@ -33,24 +33,8 @@ public class RenderHudStatusBarMixin {
         int x = this.scaledWidth / 2 + 10;
         int y = this.scaledHeight - 49;
 
-        /*
-        drawContext.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, FULL_RADIATION);
-
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-         */
-
-        try {
-            if (client.player.isSubmergedInWater()) {
-                y -= 9;
-            }
-        }
-        catch (NullPointerException e) {
-
+        if (client.player != null && client.player.isSubmergedInWater()) {
+            y -= 9;
         }
 
         for (int i = 0; i < 10; i++) {
