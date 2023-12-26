b3d7a57e771a9108216833de81a27122deca51b7
Nicholas <nici.bournes21@gmail.com>
2023-12-26 21:43:20 +0100

    add iodine pills

---
+++ src/main/java/com/bierfleisch/industrialmod/item/IndustrialModFoodComponents.java
@@ -0,0 +1,7 @@
+package com.bierfleisch.industrialmod.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class IndustrialModFoodComponents {
+    public static final FoodComponent IODINE_PILL_FOOD_COMPONENT = new FoodComponent.Builder().hunger(1).saturationModifier(0.3f).snack().alwaysEdible().build();
+}
--- src/main/java/com/bierfleisch/industrialmod/item/IndustrialModRawMaterialItems.java
+++ src/main/java/com/bierfleisch/industrialmod/item/IndustrialModRawMaterialItems.java
@@ -1,11 +1,12 @@
 package com.bierfleisch.industrialmod.item;
 
+import com.bierfleisch.industrialmod.item.food.IodinePillItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 
 public class IndustrialModRawMaterialItems {
     public static final Item RAW_URANIUM_ITEM = new Item(new FabricItemSettings());
     public static final Item RAW_PLUTONIUM_ITEM = new Item(new FabricItemSettings());
-    public static final Item IODINE_PILL_ITEM = new IodinePillItem(new FabricItemSettings());
+    public static final IodinePillItem IODINE_PILL_ITEM = new IodinePillItem();
 }
 
